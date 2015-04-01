1c57696291fc95281f71e0c535ec4bd95cbc9d0b
Hardik Bansal <hardikbansal24@gmail.com>
2015-04-01 13:06:57 +0530

    Till Tuesday

--- test/HelloWorld.java
+++ test/HelloWorld.java
@@ -2,17 +2,12 @@ class HelloWorld
 {
    public static void main(String args[])
    {
-   		int a,b;
-   		a = 15;
-   		while(a > 4)
-   		{
-   			if(a > 4)
-	   		{
-	   			a = 5;
-	   		}
-
-	   		a =6;
-
+   		int a;
+   		a = 5;
+   		for(int i =0; i < 5; i = i + 1){
+   			while(true){
+   				a = 4;
+   			}
    		}
    }
 }
