5dac65cf61c0905e1a14e716f18842aa85086a08
architrathore <architrathore1@gmail.com>
2015-04-12 18:53:31 +0530

    Freed

--- Ass4/test/if.java
+++ Ass4/test/if.java
@@ -1,12 +1,9 @@
 class HelloWorld
 {
-	public static int add(int a,int b){
-		int c = a+b;
-		return c;
-	}
 	public static int main(int k)
 	{
 		int p1 = 10, p2 =15;
-		int x = add(p1,p2);
+		int x = p2*p1;	
+		System.out.println(x);
 	}
 }
