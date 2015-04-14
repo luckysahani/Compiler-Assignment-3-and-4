57174c271658635b96cb49a8a45e4337e9055dc1
luckysahani <luckythegreat4@gmail.com>
2015-04-14 21:05:04 +0530

    finalizing the testcases

---
+++ Ass4/final_testcases/dowhile.java
@@ -0,0 +1,11 @@
+class HelloWorld
+{
+	public static int main(int a)
+	{
+		int b = 0;
+		do{
+			b = b + 1;
+		}
+		while (b < 6);
+	}
+}
--- Ass4/test/HelloWorld.java
+++ Ass4/test/HelloWorld.java
@@ -1,7 +1,7 @@
 class HelloWorld
 {
 	public static void func(int k){
-		System.out.println("Good Job Archit\n");
+		System.out.println("Good Job \n");
 	}
 	int y,x;
 	public static int main(int b){
