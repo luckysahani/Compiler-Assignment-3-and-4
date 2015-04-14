722ad30f7dedcfdcd75030ed2de6524b290efbbc
luckysahani <luckythegreat4@gmail.com>
2015-04-14 20:52:15 +0530

    testing some testcases 2

--- Ass4/test/if.java
+++ Ass4/test/if.java
@@ -5,11 +5,10 @@ class HelloWorld
         int x = 5;
         if(x == (7)-2){
             System.out.println(x);
-            string x1= "fuck you;";
+            string x1= "try;";
             System.out.println(x1);
             return 0;
             System.out.println("Please, run it on a Server!");
-            // fuck you;
         }
     }
 }
--- Ass4/test/test.java
+++ Ass4/test/test.java
@@ -6,7 +6,7 @@ class HelloWorld
 		if(x == (7)-2){
 			System.out.println(x);
 			string x1;
-			x1= "fuck you;";
+			x1= "try;";
 			System.out.println(x1);
 			return 0;
 		}
